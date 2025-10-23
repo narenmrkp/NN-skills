18d5da888259278401640be9cb83bc2adf8969ab
Narendra <159535924+narenmrkp@users.noreply.github.com>
2025-10-23 17:08:19 +0530

    Create minikube.sh

---
+++ DevOps/Tasks/K8S/minikube.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+minikube version
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+(or)
+sudo snap install kubectl --classic    (we can run above 3 lines or this single line command)
+
+kubectl version --client
+minikube start
+minikube status
+
+kubectl cluster-info
+kubectl get nodes
+kubectl run nn-pod --image nginx
+kubectl get pod -o wide
+kubectl get pods --all-namespaces
+
+minikube stop
