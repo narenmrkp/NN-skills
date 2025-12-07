5ee96633cedfd4f2e75aee8eed52512caf1a596d
Narendra <159535924+narenmrkp@users.noreply.github.com>
2025-12-07 11:05:00 +0530

    Update 11-BigMartSales_Prediction_ML_Siddhardhan.sh

--- ML/ML_Siddhardhan_Projects/11-BigMartSales_Prediction_ML_Siddhardhan.sh
+++ ML/ML_Siddhardhan_Projects/11-BigMartSales_Prediction_ML_Siddhardhan.sh
@@ -6,20 +6,26 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn import metrics
+
 # loading the data from csv file to Pandas DataFrame
 big_mart_data = pd.read_csv('/content/Train.csv')
 # first 5 rows of the dataframe
 big_mart_data.head()
+
 # number of data points & number of features
 big_mart_data.shape
+
 # getting some information about thye dataset
 big_mart_data.info()
+
 # checking for missing values
 big_mart_data.isnull().sum()
+
 # mean value of "Item_Weight" column
 big_mart_data['Item_Weight'].mean()
 # filling the missing values in "Item_weight column" with "Mean" value
 big_mart_data['Item_Weight'].fillna(big_mart_data['Item_Weight'].mean(), inplace=True)
+
 # mode of "Outlet_Size" column
 big_mart_data['Outlet_Size'].mode()
 # filling the missing values in "Outlet_Size" column with Mode
@@ -28,9 +34,11 @@ print(mode_of_Outlet_size)
 miss_values = big_mart_data['Outlet_Size'].isnull()   
 print(miss_values)
 big_mart_data.loc[miss_values, 'Outlet_Size'] = big_mart_data.loc[miss_values,'Outlet_Type'].apply(lambda x: mode_of_Outlet_size[x])
+
 # checking for missing values
 big_mart_data.isnull().sum()
 big_mart_data.describe()
+
 sns.set()
 # Item_Weight distribution
 plt.figure(figsize=(6,6))
